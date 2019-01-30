bca52ebe0b51619b1a2ad8da6ef49cf648350d78
izumimatsuo <gurux2daddy@gmail.com>
2019-01-30 23:29:58 +0900

    Delete start_privae_registry.sh

--- start_private_registry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker ps |grep registry || docker run -d -p 5000:5000 --name registry registry:2.3.0
-docker build . -t localhost:5000/ansible/test-runner
-docker push localhost:5000/ansible/test-runner
