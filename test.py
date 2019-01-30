05cfba4d344e76cf1668ae0fd14a3a84ad7dd072
izumimatsuo <gurux2daddy@gmail.com>
2019-01-31 01:26:08 +0900

    define apache role

--- test.py
+++ test.py
@@ -3,4 +3,9 @@
 def test_apache_is_installed(host):
   httpd = host.package("httpd")
   assert httpd.is_installed
+  assert httpd.version.startswith("2.4")
 
+def test_apache_running_and_enabled(host):
+  httpd = host.service("httpd")
+  assert httpd.is_running
+  assert httpd.is_enabled
