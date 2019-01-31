f0e842c7818942abbf50a5eea79ca548bb8c92b8
izumimatsuo <gurux2daddy@gmail.com>
2019-01-31 22:20:53 +0900

    define postgresql role

--- test.py
+++ test.py
@@ -9,3 +9,13 @@ def test_apache_running_and_enabled(host):
   httpd = host.service("httpd")
   assert httpd.is_running
   assert httpd.is_enabled
+
+def test_postgresql_is_installed(host):
+  postgresql = host.package("postgresql96")
+  assert postgresql.is_installed
+  assert postgresql.version.startswith("9.6")
+
+def test_postgresql_running_and_enabled(host):
+  postgresql = host.service("postgresql-9.6")
+  assert postgresql.is_running
+  assert postgresql.is_enabled
