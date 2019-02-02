e3cfd80fa880685d29ab4e9c84271708b0087af1
izumimatsuo <gurux2daddy@gmail.com>
2019-02-02 11:55:23 +0900

    update ruby role

--- ansible_build.sh
+++ ansible_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 docker build . -t test-runner
-docker run --name runner --privileged -v `pwd`:/root/build -d test-runner /sbin/init
-docker exec -it runner bash -c "cd /root/build && ansible-playbook site.yml --syntax-check"
-docker exec -it runner bash -c "cd /root/build && ansible-playbook site.yml"
-docker exec -it runner bash -c "cd /root/build && py.test -v test.py"
-docker rm -f runner
+docker ps | grep runner || docker run --name runner --privileged -v `pwd`:/root/build -d test-runner /sbin/init
+docker exec -it runner bash -lc "cd /root/build && ansible-playbook site.yml --syntax-check"
+docker exec -it runner bash -lc "cd /root/build && ansible-playbook site.yml"
+docker exec -it runner bash -lc "cd /root/build && py.test -v test.py"
+#docker rm -f runner
--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 # -*- coding:utf8 -*-
 
+# webservers tests
+
 def test_apache_is_installed(host):
-  httpd = host.package("httpd")
-  assert httpd.is_installed
-  assert httpd.version.startswith("2.4")
+    httpd = host.package("httpd")
+    assert httpd.is_installed
+    assert httpd.version.startswith("2.4")
 
 def test_apache_running_and_enabled(host):
-  httpd = host.service("httpd")
-  assert httpd.is_running
-  assert httpd.is_enabled
+    httpd = host.service("httpd")
+    assert httpd.is_running
+    assert httpd.is_enabled
+
+def test_ruby_is_installed(host):
+    assert host.exists("ruby")
+    assert "2.5" in host.run("ruby -v").stdout
+
+# dbservers tests
 
 def test_postgresql_is_installed(host):
-  postgresql = host.package("postgresql96")
-  assert postgresql.is_installed
-  assert postgresql.version.startswith("9.6")
+    postgresql = host.package("postgresql96")
+    assert postgresql.is_installed
+    assert postgresql.version.startswith("9.6")
 
 def test_postgresql_running_and_enabled(host):
-  postgresql = host.service("postgresql-9.6")
-  assert postgresql.is_running
-  assert postgresql.is_enabled
+    postgresql = host.service("postgresql-9.6")
+    assert postgresql.is_running
+    assert postgresql.is_enabled
